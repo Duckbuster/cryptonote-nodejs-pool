465bcfc3fd3403d3d0126fa22e18c61abe567f91
Duckbuster <pieterdecang@gmail.com>
2018-03-20 21:26:58 +0100

    Added favicon to website and setup config

--- website_example/config.js
+++ website_example/config.js
@@ -1,13 +1,13 @@
-var api = "http://poolhost:8117";
-var poolHost = "poolhost.com";
+var api = "https://swimmingpool.lolisandstuff.moe:8119";
+var poolHost = "swimmingpool.lolisandstuff.moe";
 
-var email = "support@poolhost.com";
-var telegram = "https://t.me/YourPool";
-var discord = "https://discordapp.com/invite/YourPool";
+var email = "support@lolisandstuff.moe";
+var telegram = "";
+var discord = "https://discord.gg/SPvKFxx";
 
 var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
 
 var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
 var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
 
-var themeCss = "themes/light.css";
+var themeCss = "themes/dark.css";
